e3b4e6b338c4e73200a3b76b336724024b7de134
Gabriela Galves <gabrielagalves@yahoo.com.br>
2015-10-02 23:21:01 -0300

    V 1.2.2
    Menu: incluso link para tela de cadastro de menu

--- codigos/menu.php
+++ codigos/menu.php
@@ -1,7 +1,7 @@
 <div id="menu">
 <div id='cssmenu'>
 <ul>
-   <li class='active'><a href='index.php'><span>Home</span></a></li>
+   <li class='active'><a href='#'><span>texto</span></a></li>
    <li class='has-sub'><a href='#'><span>Veículo</span></a>
       <ul>
          <li><a href='cadVeiculo.php'><span>Cadastrar</span></a></li>
@@ -11,9 +11,9 @@
          <li class='last'><a href='#'><span>texto</span></a></li>
       </ul>
    </li>
-   <li class='has-sub'><a href='#'><span>texto</span></a>
+   <li class='has-sub'><a href='#'><span>Cliente</span></a>
       <ul>
-         <li><a href='#'><span>texto</span></a></li>
+         <li><a href='cadCliente.php'><span>Cadastrar</span></a></li>
          <li><a href='#'><span>texto</span></a></li>
          <li><a href='#'><span>texto</span></a></li>
          <li class='last'><a href='#'><span>texto</span></a></li>
